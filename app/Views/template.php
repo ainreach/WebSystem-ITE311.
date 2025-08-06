6e7a2b635f9f3129efd0da4e54f8dad29a83d493
ainreach <ainreach123456@gmail.com>
2025-08-06 01:07:10 -0700

    Added Bootstrap template and Home controller

---
+++ app/Views/template.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bootstrap + CodeIgniter</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar" style="background-color:#8B0000;">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1 text-white">ITE311-NABALE</span>
+  </div>
+</nav>
+
+<div class="text-center mt-5">
+  <h2>Welcome to my Bootstrap + CodeIgniter Page!</h2>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
